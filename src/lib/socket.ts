3ac0bdc7b76c2987ee571106c0da4ec7df39d585
Reda Bouziani <bouziani.reda.1201@gmail.com>
2025-02-09 21:41:22 +0100

    second version

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 // Create socket instance with error handling
 
-const API_URL = 'Your_API'; 
+const API_URL = 'http://127.0.0.1:5000'; 
 export const socket = io(API_URL, {
   transports: ['websocket'],
   reconnection: true,
